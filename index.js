4cd27b5f857386e447e6301f502290fc05fc29b1
Aleksey Tyshkul <oty872020@gmail.com>
2020-06-17 18:50:46 +0300

    flexbox added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-    console.log(key);
+function playSound(event) {
+    const audio = document.querySelector(`audio[id="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[id="${event.keyCode}"]`);
     if (!audio) return; // stop function
     audio.currentTime = 0;
     audio.play();
@@ -18,3 +17,13 @@ keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
 
+
+
+function playClick(e) {
+    const audio = document.querySelector(`audio[id="${e}"]`);
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const key = document.querySelectorAll(`.key`);
+key.forEach(playClick);
